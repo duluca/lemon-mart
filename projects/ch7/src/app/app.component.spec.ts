95437082061b5dd0e950fa7f24d23f541b0a8e34
Doguhan Uluca <duluca@gmail.com>
2020-04-05 07:59:58 -0400

    test fix

--- projects/ch7/src/app/app.component.spec.ts
+++ projects/ch7/src/app/app.component.spec.ts
@@ -1,12 +1,27 @@
 import { TestBed, async } from '@angular/core/testing'
+import { MediaObserver } from '@angular/flex-layout'
+import { MatIconRegistry } from '@angular/material/icon'
+import { DomSanitizer } from '@angular/platform-browser'
 import { RouterTestingModule } from '@angular/router/testing'
 
 import { AppComponent } from './app.component'
+import {
+  DomSanitizerFake,
+  MatIconRegistryFake,
+  MediaObserverFake,
+  commonTestingProviders,
+} from './common/common.testing'
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
+      providers: commonTestingProviders.concat([
+        { provide: MediaObserver, useClass: MediaObserverFake },
+        { provide: MatIconRegistry, useClass: MatIconRegistryFake },
+        { provide: DomSanitizer, useClass: DomSanitizerFake },
+      ]),
+
       declarations: [AppComponent],
     }).compileComponents()
   }))
@@ -17,18 +32,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy()
   })
 
-  it(`should have as title 'ch7'`, () => {
-    const fixture = TestBed.createComponent(AppComponent)
-    const app = fixture.componentInstance
-    expect(app.title).toEqual('ch7')
-  })
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent)
     fixture.detectChanges()
     const compiled = fixture.nativeElement
-    expect(compiled.querySelector('.content span').textContent).toContain(
-      'ch7 app is running!'
-    )
+    expect(compiled.querySelector('span.mat-h2').textContent).toContain('LemonMart')
   })
 })
