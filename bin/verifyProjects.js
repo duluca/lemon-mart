d78a126406e571bbe881bb4de4859c3048eaaa78
Doguhan Uluca <duluca@gmail.com>
2020-04-06 18:05:17 -0400

    added verify projects script

---
+++ bin/verifyProjects.js
@@ -0,0 +1,37 @@
+const cp = require('child_process')
+const fs = require('fs')
+const path = require('path')
+
+// get projects root
+const root = path.resolve(__dirname, '../projects/')
+
+fs.readdirSync(root).forEach((module) => {
+  const modulePath = path.join(root, module)
+  console.log(modulePath)
+
+  const skipTests = module === ''
+
+  const project = ['--project', module]
+
+  const build = ['ng', 'build', '--prod'].concat(project)
+  const test = ['ng', 'test', '--watch=false'].concat(project)
+  const e2e = ['ng', 'e2e', module]
+
+  const npmCommandsArray = skipTests ? [build] : [build, test, e2e]
+
+  npmCommandsArray.forEach((command) => {
+    const spawn = cp.spawnSync(
+      /^win/.test(process.platform) ? 'npx.cmd' : 'npx',
+      command,
+      {
+        env: process.env,
+        cwd: modulePath,
+        stdio: 'inherit',
+      }
+    )
+
+    if (spawn.status != 0) {
+      throw `Error: npx ${command.join(' ')} failed`
+    }
+  })
+})
