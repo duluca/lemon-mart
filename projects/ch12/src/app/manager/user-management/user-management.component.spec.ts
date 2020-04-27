9f5a8c6e29baa375a6ed2d98d4c27cc1e75c7395
Doguhan Uluca <duluca@gmail.com>
2020-04-27 05:29:51 -0400

    tests

--- projects/ch12/src/app/manager/user-management/user-management.component.spec.ts
+++ projects/ch12/src/app/manager/user-management/user-management.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed, async } from '@angular/core/testing'
 
+import { commonTestingModules, commonTestingProviders } from '../../common/common.testing'
+import { ManagerMaterialModule } from '../manager-material.module'
 import { UserManagementComponent } from './user-management.component'
 
 describe('UserManagementComponent', () => {
@@ -8,6 +10,8 @@ describe('UserManagementComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      providers: commonTestingProviders,
+      imports: commonTestingModules.concat([ManagerMaterialModule]),
       declarations: [UserManagementComponent],
     }).compileComponents()
   }))
