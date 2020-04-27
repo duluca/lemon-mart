d289190526512b812bf4eb5412c79d2e6066b176
Doguhan Uluca <duluca@gmail.com>
2020-04-27 02:22:15 -0400

    style fix

--- projects/ch11/src/app/user/user/user.service.ts
+++ projects/ch11/src/app/user/user/user.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 import { Observable, throwError } from 'rxjs'
-import { catchError, map, tap } from 'rxjs/operators'
+import { catchError, map } from 'rxjs/operators'
 
 import { environment } from '../../../environments/environment'
 import { AuthService } from '../../auth/auth.service'
