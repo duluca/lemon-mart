47742d21cbf74ffff96e56022214ebcb55b36018
Doguhan Uluca <duluca@gmail.com>
2020-04-28 19:20:21 -0400

    fix ch14

--- projects/ch14/src/app/pos/pos/pos.component.spec.ts
+++ projects/ch14/src/app/pos/pos/pos.component.spec.ts
@@ -1,24 +1,38 @@
 import { ComponentFixture, TestBed, async } from '@angular/core/testing'
+import { autoSpyObj, injectSpy } from 'angular-unit-test-helper'
+import { of } from 'rxjs'
 
+import { commonTestingModules } from '../../common/common.testing'
+import { commonTestingProviders } from '../../common/common.testing'
+import { TransactionService } from '../transaction/transaction.service'
 import { PosComponent } from './pos.component'
 
 describe('PosComponent', () => {
   let component: PosComponent
   let fixture: ComponentFixture<PosComponent>
+  let transactionServiceMock: jasmine.SpyObj<TransactionService>
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [PosComponent],
-    }).compileComponents()
+      providers: commonTestingProviders.concat([
+        { provide: TransactionService, useValue: autoSpyObj(TransactionService) },
+      ]),
+      imports: commonTestingModules,
+    }).compileComponents(),
+      (transactionServiceMock = injectSpy(TransactionService))
   }))
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PosComponent)
     component = fixture.componentInstance
-    fixture.detectChanges()
   })
 
   it('should create', () => {
+    transactionServiceMock.processTransaction.and.returnValue(
+      of('5a6352c6810c19729de860ea')
+    )
+    fixture.detectChanges()
     expect(component).toBeTruthy()
   })
 })
