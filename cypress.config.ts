d686388d3e41fadb5d68a0671ec2776c06b25454
Doguhan Uluca <duluca@gmail.com>
2023-08-10 00:20:37 -0400

    added cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,21 +1,21 @@
 export default {
-  projectId: "2jeenu",
+  projectId: '2jeenu',
 
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require("./cypress/plugins/index.js")(on, config);
+    setupNodeEvents(on: any, config: any) {
+      return require('./cypress/plugins/index.js')(on, config)
     },
-    baseUrl: "https://lemonmart.angularforenterprise.com",
-    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
+    baseUrl: 'https://lemonmart.angularforenterprise.com',
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
   },
 
   component: {
     devServer: {
-      framework: "angular",
-      bundler: "webpack",
+      framework: 'angular',
+      bundler: 'webpack',
     },
-    specPattern: "**/*.cy.ts",
+    specPattern: '**/*.cy.ts',
   },
-};
+}
