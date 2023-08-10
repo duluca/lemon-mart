2fe34ef5a963d3bce4de6391bb5cbf68efc6abe3
Doguhan Uluca <duluca@gmail.com>
2023-08-10 00:01:16 -0400

    cypress update

---
+++ cypress.config.ts
@@ -0,0 +1,21 @@
+export default {
+  projectId: "2jeenu",
+
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require("./cypress/plugins/index.js")(on, config);
+    },
+    baseUrl: "https://lemonmart.angularforenterprise.com",
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
+  },
+
+  component: {
+    devServer: {
+      framework: "angular",
+      bundler: "webpack",
+    },
+    specPattern: "**/*.cy.ts",
+  },
+};
--- cypress/integration/spec.js
+++
@@ -1,7 +0,0 @@
-/// <reference types="cypress" />
-it('works', () => {
-  cy.visit('/')
-  cy.get('[aria-label="E-mail"]').type('cashier@test.com')
-  cy.get('[aria-label="Password"]').type('12345678')
-  cy.contains('Login').click()
-})
