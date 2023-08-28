74da24e728c3821ff7cb5755888e8799a4bab68c
Doguhan Uluca <duluca@gmail.com>
2023-08-28 14:56:06 -0400

    tests work

--- karma.conf.js
+++ karma.conf.js
@@ -9,13 +9,14 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-junit-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma'),
     ],
     client: {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
+    coverageReporter: {
       dir: require('path').join(__dirname, 'coverage'),
       reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true,
@@ -23,31 +24,17 @@ module.exports = function (config) {
     angularCli: {
       environment: 'dev',
     },
-    reporters: ['progress', 'kjhtml'],
-    htmlReporter: {
-      outputFile: 'tests/units.html',
+    reporters: ['progress', 'kjhtml', 'junit'],
+    junitReporter: {
+      outputDir: 'test_results',
+      useBrowserName: true, // add browser name to report and classes names
+      xmlVersion: null, // use '1' if reporting to be per SonarQube 6.2 XML format
     },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome', 'ChromiumHeadless', 'ChromiumNoSandbox'],
-    customLaunchers: {
-      ChromiumHeadless: {
-        base: 'Chrome',
-        flags: [
-          '--headless',
-          '--disable-gpu',
-          // Without a remote debugging port, Google Chrome exits immediately.
-          '--remote-debugging-port=9222',
-        ],
-        debug: true,
-      },
-      ChromiumNoSandbox: {
-        base: 'ChromiumHeadless',
-        flags: ['--no-sandbox', '--disable-translate', '--disable-extensions'],
-      },
-    },
+    browsers: ['Chrome'],
     singleRun: false,
   })
 }
