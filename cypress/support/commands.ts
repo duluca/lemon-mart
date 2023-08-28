a75427b786dfbaf707909f98a6bbf7ee962e2dc3
Doguhan Uluca <duluca@gmail.com>
2023-08-28 13:06:49 -0400

    update ci, rename stages, and cleanup

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,6 +1,24 @@
-/// <reference types="cypress" />
+/* eslint-disable @typescript-eslint/no-namespace */
 // ***********************************************
-// This example commands.ts shows you how to
+// This example namespace declaration will help
+// with Intellisense and code completion in your
+// IDE or Text Editor.
+// ***********************************************
+// declare namespace Cypress {
+//   interface Chainable<Subject = any> {
+//     customCommand(param: any): typeof customCommand;
+//   }
+// }
+//
+// function customCommand(param: any): void {
+//   console.warn(param);
+// }
+//
+// NOTE: You can use it like so:
+// Cypress.Commands.add('customCommand', customCommand);
+//
+// ***********************************************
+// This example commands.js shows you how to
 // create various custom commands and overwrite
 // existing commands.
 //
@@ -11,27 +29,45 @@
 //
 //
 // -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
+// Cypress.Commands.add("login", (email, password) => { ... })
 //
 //
 // -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
 //
 //
 // -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
 //
 //
 // -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
+// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+// Read more at https://testing-angular.com/end-to-end-testing/#end-to-end-testing
+declare namespace Cypress {
+  interface Chainable {
+    /**
+     * Get one or more DOM elements by test id.
+     *
+     * @param id The test id
+     * @param options The same options as cy.get
+     */
+    byTestId<E extends Node = HTMLElement>(
+      id: string,
+      options?: Partial<
+        Cypress.Loggable & Cypress.Timeoutable & Cypress.Withinable & Cypress.Shadow
+      >
+    ): Cypress.Chainable<JQuery<E>>
+  }
+}
+
+Cypress.Commands.add(
+  'byTestId',
+  // Borrow the signature from cy.get
+  <E extends Node = HTMLElement>(
+    id: string,
+    options?: Partial<
+      Cypress.Loggable & Cypress.Timeoutable & Cypress.Withinable & Cypress.Shadow
+    >
+  ): Cypress.Chainable<JQuery<E>> => cy.get(`[data-testid="${id}"]`, options)
+)
--- test.Dockerfile
+++ test.Dockerfile
@@ -9,9 +9,6 @@ COPY . .
 # install dependencies and build
 RUN npm ci
 
-# force update the webdriver, so it runs with latest version of Chrome
-RUN cd ./node_modules/protractor && npm i webdriver-manager@latest
-
 WORKDIR /usr/src
 
 RUN npm run test:prod
