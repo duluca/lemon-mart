86476cc27916b5269e63c99836d32082dd5fcf51
Doguhan Uluca <duluca@gmail.com>
2023-09-08 02:36:15 -0400

    script update

--- replicate_batch.sh
+++ replicate_batch.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# check git status clean before replicating, exit if not clean
+if [[ -n $(git status -s) ]]; then
+  echo "Git status is not clean, please commit changes before replicating"
+  exit 1
+fi
+
 echo "Replicating..."
 
 # ./replicate.sh src/app/common "ui.service.ts ui.service.spec.ts" "8 10 11 12 13 14"
