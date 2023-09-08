5c1f0fafce72c91349f2c6042635a3c32f7d49be
Doguhan Uluca <duluca@gmail.com>
2023-09-08 03:12:28 -0400

    update to main

--- projects/stage12/src/main.ts
+++ projects/stage12/src/main.ts
@@ -1,59 +1,12 @@
-import {
-  HTTP_INTERCEPTORS,
-  HttpClient,
-  provideHttpClient,
-  withInterceptorsFromDi,
-} from '@angular/common/http'
-import { enableProdMode, importProvidersFrom } from '@angular/core'
-import { provideFirebaseApp } from '@angular/fire/app'
-import { getAuth, provideAuth } from '@angular/fire/auth'
-import { ReactiveFormsModule } from '@angular/forms'
-import { bootstrapApplication, BrowserModule } from '@angular/platform-browser'
-import { provideAnimations } from '@angular/platform-browser/animations'
-import { FlexLayoutModule } from '@ngbracket/ngx-layout'
-import { EntityDataModule } from '@ngrx/data'
-import { EffectsModule } from '@ngrx/effects'
-import { StoreModule } from '@ngrx/store'
-import { StoreDevtoolsModule } from '@ngrx/store-devtools'
-import { initializeApp } from 'firebase/app'
+import { enableProdMode } from '@angular/core'
+import { bootstrapApplication } from '@angular/platform-browser'
 
 import { AppComponent } from './app/app.component'
-import { AppRoutingModule } from './app/app-routing.module'
-import { authFactory } from './app/auth/auth.factory'
-import { AuthService } from './app/auth/auth.service'
-import { AuthHttpInterceptor } from './app/auth/auth-http-interceptor'
-import { entityConfig } from './app/entity-metadata'
+import { appConfig } from './app/app.config'
 import { environment } from './environments/environment'
 
 if (environment.production) {
   enableProdMode()
 }
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    importProvidersFrom(
-      BrowserModule,
-      AppRoutingModule,
-      FlexLayoutModule,
-      ReactiveFormsModule,
-      provideFirebaseApp(() => initializeApp(environment.firebase)),
-      provideAuth(() => getAuth()),
-      StoreModule.forRoot({}, {}),
-      EffectsModule.forRoot([]),
-      EntityDataModule.forRoot(entityConfig),
-      StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
-    ),
-    {
-      provide: AuthService,
-      useFactory: authFactory,
-      deps: [HttpClient],
-    },
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthHttpInterceptor,
-      multi: true,
-    },
-    provideAnimations(),
-    provideHttpClient(withInterceptorsFromDi()),
-  ],
-}).catch((err) => console.error(err))
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.log(err))
--- projects/stage8/src/main.ts
+++ projects/stage8/src/main.ts
@@ -1,51 +1,12 @@
-import {
-  HTTP_INTERCEPTORS,
-  provideHttpClient,
-  withInterceptorsFromDi,
-} from '@angular/common/http'
-import { enableProdMode, importProvidersFrom } from '@angular/core'
-import { provideFirebaseApp } from '@angular/fire/app'
-import { provideAuth } from '@angular/fire/auth'
-import { ReactiveFormsModule } from '@angular/forms'
-import { bootstrapApplication, BrowserModule } from '@angular/platform-browser'
-import { provideAnimations } from '@angular/platform-browser/animations'
-import { FlexLayoutModule } from '@ngbracket/ngx-layout'
-import { initializeApp } from 'firebase/app'
-import { getAuth } from 'firebase/auth'
+import { enableProdMode } from '@angular/core'
+import { bootstrapApplication } from '@angular/platform-browser'
 
 import { AppComponent } from './app/app.component'
-import { AppRoutingModule } from './app/app-routing.module'
-import { authFactory } from './app/auth/auth.factory'
-import { AuthService } from './app/auth/auth.service'
-import { AuthHttpInterceptor } from './app/auth/auth-http-interceptor'
+import { appConfig } from './app/app.config'
 import { environment } from './environments/environment'
 
 if (environment.production) {
   enableProdMode()
 }
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    importProvidersFrom(
-      BrowserModule,
-      AppRoutingModule,
-      FlexLayoutModule,
-      ReactiveFormsModule,
-      provideFirebaseApp(() => initializeApp(environment.firebase)),
-      provideAuth(() => getAuth())
-    ),
-    {
-      provide: AuthService,
-      useFactory: authFactory,
-      deps: [],
-      // useClass: InMemoryAuthService,
-    },
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthHttpInterceptor,
-      multi: true,
-    },
-    provideAnimations(),
-    provideHttpClient(withInterceptorsFromDi()),
-  ],
-}).catch((err) => console.error(err))
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.log(err))
