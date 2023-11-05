9e271a190bf06ac55dcaf7a1948576d6d92f09a8
Doguhan Uluca <duluca@gmail.com>
2023-11-05 13:11:21 -0500

    expression changed fix

--- src/app/manager/user-table/user-table.component.ts
+++ src/app/manager/user-table/user-table.component.ts
@@ -9,7 +9,7 @@ import { MatInputModule } from '@angular/material/input'
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator'
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
 import { MatSlideToggleModule } from '@angular/material/slide-toggle'
-import { MatSort, MatSortModule, SortDirection } from '@angular/material/sort'
+import { MatSort, MatSortModule } from '@angular/material/sort'
 import { MatTableModule } from '@angular/material/table'
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { RouterLink } from '@angular/router'
@@ -19,7 +19,7 @@ import { catchError, debounceTime, map, startWith, switchMap } from 'rxjs/operat
 
 import { OptionalTextValidation } from '../../common/validations'
 import { IUser } from '../../user/user/user'
-import { IUsers, UserService } from '../../user/user/user.service'
+import { UserService } from '../../user/user/user.service'
 
 @Component({
   selector: 'app-user-table',
@@ -53,7 +53,7 @@ export class UserTableComponent implements AfterViewInit {
   errorText = ''
   private skipLoading = false
   private destroyRef = inject(DestroyRef)
-  useNgRxData = false
+
   readonly isLoadingResults$ = new BehaviorSubject(true)
   loading$: Observable<boolean>
   refresh$ = new Subject<void>()
@@ -76,39 +76,48 @@ export class UserTableComponent implements AfterViewInit {
       return
     }
 
-    this.items$ = merge(
-      this.refresh$,
-      this.sort.sortChange,
-      this.paginator.page,
-      this.search.valueChanges.pipe(debounceTime(1000))
-    ).pipe(
-      takeUntilDestroyed(this.destroyRef),
-      startWith({}),
-      switchMap(() => {
-        this.isLoadingResults$.next(true)
-        return this.userService.getUsers(
-          this.paginator.pageSize,
-          this.search.value as string,
-          this.paginator.pageIndex,
-          this.sort.active,
-          this.sort.direction
-        )
-      }),
-      map((results: { total: number; data: IUser[] }) => {
-        console.log(results)
-        this.isLoadingResults$.next(false)
-        this.hasError = false
-        this.resultsLength = results.total
+    setTimeout(() => {
+      this.items$ = merge(
+        this.refresh$,
+        this.sort.sortChange,
+        this.paginator.page,
+        this.search.valueChanges.pipe(debounceTime(1000))
+      ).pipe(
+        takeUntilDestroyed(this.destroyRef),
+        startWith({}),
+        switchMap(() => {
+          this.isLoadingResults$.next(true)
+          console.log({
+            size: this.paginator.pageSize,
+            search: this.search.value as string,
+            skip: this.paginator.pageIndex,
+            sort: this.sort.active,
+            direction: this.sort.direction,
+          })
 
-        return results.data
-      }),
-      catchError((err) => {
-        this.isLoadingResults$.next(false)
-        this.hasError = true
-        this.errorText = err
-        return of([])
-      })
-    )
-    this.items$.subscribe()
+          return this.userService.getUsers(
+            this.paginator.pageSize,
+            this.search.value as string,
+            this.paginator.pageIndex,
+            this.sort.active,
+            this.sort.direction
+          )
+        }),
+        map((results: { total: number; data: IUser[] }) => {
+          console.log(results)
+          this.isLoadingResults$.next(false)
+          this.hasError = false
+          this.resultsLength = results.total
+
+          return results.data
+        }),
+        catchError((err) => {
+          this.isLoadingResults$.next(false)
+          this.hasError = true
+          this.errorText = err
+          return of([])
+        })
+      )
+    })
   }
 }
